e34aa201410d4eb604d274b22c96323f73c8d631
Nachop51 <nachoperalta0@gmail.com>
2022-03-11 18:15:12 +0000

    tests

--- print.c
+++ print.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-	int n = 10;
-	char *str = "HSLJLHS";
+	int n = 234567897;
+	char *str = "ASDsadsad";
 
-	_printf("Hello %d\n", n);
+	_printf("Hello %d, %s\n", n, str);
 	_printf("Hello %s\n", str);
 	return (0);
 }
